444490a5eab606d8903584719b1ff2a5f5e84277
jc462737 <brandon.paull@my.jcu.edu>
2021-10-25 14:42:15 +1000

    Fixed Formatting for weather_station.py

--- weather_station.py
+++ weather_station.py
@@ -1,9 +1,10 @@
 class Weather_station:
-    def __init__(self, weather_station_name, weather_station_location_x, weather_station_location_y, sunlight_exposure, rain_fall, wind_speeds, wind_direction):
+    def __init__(self, weather_station_name, weather_station_location_x, weather_station_location_y, sunlight_exposure,
+                 rain_fall, wind_speeds, wind_direction):
         self.weather_station_name = weather_station_name
         self.weather_station_location_x = weather_station_location_x
         self.weather_station_location_y = weather_station_location_y
-        self. sunlight_exposure = sunlight_exposure
+        self.sunlight_exposure = sunlight_exposure
         self.rain_fall = rain_fall
         self.wind_speeds = wind_speeds
         self.wind_direction = wind_direction
