b869f239f71c180360a09bef29ec41b40e0d2f8c
Sam <sam.hayes1999@gmail.com>
2021-10-30 00:03:01 +1000

    Update changes for 29/10/21 with screens.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from datetime import datetime
+# from pytz import timezone
 
 # https://realpython.com/mobile-app-kivy-python/ provides step by step guide for making an android app from kivy
 
@@ -21,36 +22,6 @@ class WelcomeWindow(Screen):
     # Create instance of database class
     global database1
     database1 = Database.DatabaseClass()
-    #global valid_station
-    #valid_station = False
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #
-    #
-    #     # Create window
-    #     Window.size = (400, 600)
-    #
-    #     self.ids.DescriptionLabel = Label(text='Enter Location:', size_hint=(.1, .1),
-    #                                   pos_hint={'center_x': 0.2, 'center_y': .98})
-    #     self.ids.UserYInput = TextInput(size_hint=(.3, .1), pos_hint={'center_x': 0.6, 'center_y': .9})
-    #     self.ids.UserXInput = TextInput(size_hint=(.3, .1), pos_hint={'center_x': 0.25, 'center_y': .9})
-    #     self.ids.locationlabel = Label(text='Location Name:', size_hint=(.2, .2),
-    #                                pos_hint={'center_x': 0.2, 'center_y': .8})
-    #     self.ids.NameInput = TextInput(size_hint=(.3, .1), pos_hint={'center_x': 0.5, 'center_y': .8})
-    #
-    #     # Add widgets and specify size/position
-    #     button = Button(text='Search',
-    #                     size_hint=(.15, .1),
-    #                     pos_hint={'center_x': 0.85, 'center_y': .9})
-    #     # Set button action
-    #     button.bind(on_press=self.ids.on_press_button)
-    #
-    #     self.ids.add_widget(self.ids.DescriptionLabel)
-    #     self.ids.add_widget(self.ids.locationlabel)
-    #     self.ids.add_widget(self.ids.NameInput)
-    #     self.ids.add_widget(self.ids.UserXInput)
-    #     self.ids.add_widget(self.ids.UserYInput)
-    #     self.ids.add_widget(button)
 
 
     def on_press_button(self):
@@ -65,7 +36,8 @@ class WelcomeWindow(Screen):
 
         # location name entry
         if self.ids.NameInput.text !='':
-            user_location = self.ids.NameInput.text.upper()
+            user_location = self.ids.NameInput.text.upper().strip()
+            self.ids.NameInput.text = self.ids.NameInput.text.title().strip()
             station = database1.determine_best_location_name(user_location=user_location)
             if not station:
                 # self.ids.load_data()
@@ -105,56 +77,6 @@ class MainWindow(Screen):
         # load data
         self.load_data()
 
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #
-    #     # Create instance of database class
-    #     # self.ids.database1 = Database.DatabaseClass()
-    #
-    #     # Create window
-    #     Window.size = (400, 600)
-    #     #layout = FloatLayout(size=(350, 250))
-    #
-    #     # Create widgets
-    #     self.ids.SkyLabel = Label(text='Condition:', size_hint=(.2, .2), pos_hint={'center_x': 0.5, 'center_y': .6})
-    #     self.ids.TimeLabel = Label(text='Time', size_hint=(.2, .2), pos_hint={'center_x': 0.5, 'center_y': .7})
-    #     self.ids.RainLabel = Label(text='Rain', size_hint=(.2, .2), pos_hint={'center_x': 0.5, 'center_y': .4})
-    #     self.ids.WindLabel = Label(text='Wind', size_hint=(.2, .2), pos_hint={'center_x': 0.2, 'center_y': .3})
-    #     self.ids.directionlabel = Label(text='Wind Direction', size_hint=(.2, .2), pos_hint={'center_x': 0.65, 'center_y': .3})
-    #     self.ids.TemperatureLabel = Label(text='Temperature:', size_hint=(.2, .2),
-    #                                   pos_hint={'center_x': 0.5, 'center_y': .5})
-    #     self.ids.DescriptionLabel = Label(text='Enter Location:', size_hint=(.1, .1),
-    #                                   pos_hint={'center_x': 0.2, 'center_y': .98})
-    #     self.ids.UserYInput = TextInput(size_hint=(.3, .1), pos_hint={'center_x': 0.6, 'center_y': .9})
-    #     self.ids.UserXInput = TextInput(size_hint=(.3, .1), pos_hint={'center_x': 0.25, 'center_y': .9})
-    #     self.ids.locationlabel = Label(text='Location Name:', size_hint=(.2, .2), pos_hint={'center_x':0.2, 'center_y': .8})
-    #     self.ids.NameInput = TextInput(size_hint=(.3, .1), pos_hint={'center_x': 0.5, 'center_y': .8})
-
-    # Create instance of database class
-    # self.ids.database1 = Database.DatabaseClass()
-
-    # Add widgets and specify size/position
-    # button = Button(text='Search',
-    #                 size_hint=(.15, .1),
-    #                 pos_hint={'center_x': 0.85, 'center_y': .9})
-    # # Set button action
-    # button.bind(on_press=self.ids.on_press_button)
-
-    # # Add widgets to the floating layout
-    # self.ids.add_widget(self.ids.DescriptionLabel)
-    # self.ids.add_widget(self.ids.locationlabel)
-    # self.ids.add_widget(self.ids.NameInput)
-    # self.ids.add_widget(self.ids.UserXInput)
-    # self.ids.add_widget(self.ids.UserYInput)
-    # # self.ids.add_widget(button)
-    # self.ids.add_widget(self.ids.TemperatureLabel)
-    # self.ids.add_widget(self.ids.SkyLabel)
-    # self.ids.add_widget(self.ids.TimeLabel)
-    # self.ids.add_widget(self.ids.RainLabel)
-    # self.ids.add_widget(self.ids.WindLabel)
-    # self.ids.add_widget(self.ids.directionlabel)
-
-
     def load_data(self):
         # insert utilising database here (include error checking etc)
         # based on the current database this will use an x&y input? (or make to have option of name or xy input)
@@ -167,11 +89,6 @@ class MainWindow(Screen):
         records = database1.get_all_relevant_current_data()  # get weather data for nearest location
 
         # Display the data
-        # self.ids.TemperatureLabel.text = 'Temperature:' + temp
-        # self.ids.TimeLabel.text = 'Time:' + time
-
-        #self.ids.SkyLabel.text = 'Condition:' + str(records.get_sunlight_exposure())
-
         self.ids.TemperatureLabel.text = 'Temperature:' + str(records.get_temperature()) + "C"
         self.ids.TimeLabel.text = 'Time:' + datetime.now().strftime("%H:%M:%S")
         if records.get_sunlight_exposure() < 25:
@@ -187,38 +104,21 @@ class MainWindow(Screen):
 
 
 class WindowManager(ScreenManager):
+    # make a global kivy accessible variable
     valid_station = False
     pass
 
-
+# load kivy build file with widgets and layouts
 kv = Builder.load_file("screens.kv")
 
 
 class WeatherLayout(App):
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     # TODO make database1 accessible between each screen
-    #     #global database1 = Database.DatabaseClass()
 
     def build(self):
         return kv
 
         # Create a screen manager
-        # self.ids.screen_manager = ScreenManager()
-
-        # # Welcome Screen
-        # self.ids.welcome_page = WelcomeScreen()
-        # screen = Screen(name='WelcomeScreen')
-        # screen.add_widget(self.ids.welcome_page)
-        # self.ids.screen_manager.add_widget(screen)
-        #
-        # # Main Menu
-        # self.ids.main_page = MainScreen()
-        # screen = Screen(name='MainMenu')
-        # screen.add_widget(self.ids.main_page)
-        # self.ids.screen_manager.add_widget(screen)
-
-        return self.ids.screen_manager
+        # return self.ids.screen_manager
 
 if __name__ == '__main__':
     app = WeatherLayout()
